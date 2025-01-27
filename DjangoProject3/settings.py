4ebc2155b3a45813862fbe01af0b2ed68be375f7
Abdul Mueed <abdulmueed235@gmail.com>
2025-01-28 00:02:58 +0500

    reuirements_updated

--- DjangoProject3/settings.py
+++ DjangoProject3/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-import environ
+import os
 import django_heroku
 from pathlib import Path
 from credentials import logindetails
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = logindetails.key
+SECRET_KEY = os.environ.get("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG","false").lower()=="true"
 
 ALLOWED_HOSTS = ["*"]
 
