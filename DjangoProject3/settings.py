c7fde4f72f6cf0e8b726f2e039f2e481df6a63d5
Abdul Mueed <abdulmueed235@gmail.com>
2025-01-28 17:50:46 +0500

    non

--- DjangoProject3/settings.py
+++ DjangoProject3/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 from credentials import logindetails
 import dj_database_url
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
